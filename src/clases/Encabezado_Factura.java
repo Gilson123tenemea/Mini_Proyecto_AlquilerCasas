725539cc6c5bcf85441af373bf523bacafa5a377
Gilson123tenemea <tenemeaaguilar@gmail.com>
2024-01-19 16:56:12 -0500

    clase

--- src/clases/Encabezado_Factura.java
+++ src/clases/Encabezado_Factura.java
@@ -19,17 +19,24 @@ public class Encabezado_Factura {
     private String cod_casa;
     private Date fecha;
     private double valor_cancelar;
-
+    private String cod_reserva;
+    private String cod_servicio;
+    private String cod_promocion;
+    private String doc_ser_adici;
     public Encabezado_Factura() {
     }
 
-    public Encabezado_Factura(String codigo_fac, String cod_cliente, String cod_agente, String cod_casa, Date fecha, double valor_cancelar) {
+    public Encabezado_Factura(String codigo_fac, String cod_cliente, String cod_agente, String cod_casa, Date fecha, double valor_cancelar, String cod_reserva, String cod_servicio, String cod_promocion, String doc_ser_adici) {
         this.codigo_fac = codigo_fac;
         this.cod_cliente = cod_cliente;
         this.cod_agente = cod_agente;
         this.cod_casa = cod_casa;
         this.fecha = fecha;
         this.valor_cancelar = valor_cancelar;
+        this.cod_reserva = cod_reserva;
+        this.cod_servicio = cod_servicio;
+        this.cod_promocion = cod_promocion;
+        this.doc_ser_adici = doc_ser_adici;
     }
 
     public String getCodigo_fac() {
@@ -80,9 +87,43 @@ public class Encabezado_Factura {
         this.valor_cancelar = valor_cancelar;
     }
 
+    public String getCod_reserva() {
+        return cod_reserva;
+    }
+
+    public void setCod_reserva(String cod_reserva) {
+        this.cod_reserva = cod_reserva;
+    }
+
+    public String getCod_servicio() {
+        return cod_servicio;
+    }
+
+    public void setCod_servicio(String cod_servicio) {
+        this.cod_servicio = cod_servicio;
+    }
+
+    public String getCod_promocion() {
+        return cod_promocion;
+    }
+
+    public void setCod_promocion(String cod_promocion) {
+        this.cod_promocion = cod_promocion;
+    }
+
+    public String getDoc_ser_adici() {
+        return doc_ser_adici;
+    }
+
+    public void setDoc_ser_adici(String doc_ser_adici) {
+        this.doc_ser_adici = doc_ser_adici;
+    }
+
     @Override
     public String toString() {
-        return "Encabezado_Factura{" + "codigo_fac=" + codigo_fac + ", cod_cliente=" + cod_cliente + ", cod_agente=" + cod_agente + ", cod_casa=" + cod_casa + ", fecha=" + fecha + ", valor_cancelar=" + valor_cancelar + '}';
+        return "Encabezado_Factura{" + "codigo_fac=" + codigo_fac + ", cod_cliente=" + cod_cliente + ", cod_agente=" + cod_agente + ", cod_casa=" + cod_casa + ", fecha=" + fecha + ", valor_cancelar=" + valor_cancelar + ", cod_reserva=" + cod_reserva + ", cod_servicio=" + cod_servicio + ", cod_promocion=" + cod_promocion + ", doc_ser_adici=" + doc_ser_adici + '}';
     }
+
+   
 
 }
